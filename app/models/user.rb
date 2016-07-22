641365c8400c3efe7c9632ec1514ba671db9f7c3
Marie Denney <mldenne@gmail.com>
2016-07-22 10:50:20 -0400

    Updated user model and serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  validates :auth_token, presence: true, uniqueness: true
+
+  before_validation :generate_auth_token, on: :create
+
+  private
+
+  def generate_auth_token
+    while auth_token.blank? || User.exists?(auth_token: auth_token)
+      self.auth_token = SecureRandom.hex(10)
+    end
+  end
+
 end
